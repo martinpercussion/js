4674e7f0c2955e4e939ef7187938a88fecbffcde
Luis Martin Rodriguez <martinpercussion@gmail.com>
2021-09-04 13:57:11 -0300

    test js

---
+++ desafioUno.js
@@ -0,0 +1,23 @@
+var alturaPared = parseFloat (prompt ("que altura posee la pared a realizar?"))
+var anchoPared = parseFloat (prompt ("cual es el ancho de la pared a realizar?"))
+var costoPorMetro = parseInt (5000)
+var superficieTotal = (alturaPared * anchoPared)
+var presupuestoTotal = (superficieTotal *  costoPorMetro)
+
+alert ("el costo total de la obra a realizar es de : $ " + presupuestoTotal)
+
+
+
+
+
+
+
+
+
+/* 
+var Presupuesto = prompt ("cuando dinero le gustaría gastar en esta etapa?")
+var PresupuestoAmbiente = (SuperficieAmbiente) * 250000 // superficie * costo x mt2
+var PresupuestoPared = (SuperficiePared) * 25000 // costo x m2
+ 
+
+ */
